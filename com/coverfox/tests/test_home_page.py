9877a075f6410b4a769ce917a63ecb509aff9563
sourabhdhingra <sourabhdhingra91@gmail.com>
2019-11-04 17:19:28 +0530

    Usign Fixtures as Function arguments to set up the page object in test page

--- com/coverfox/tests/test_home_page.py
+++ com/coverfox/tests/test_home_page.py
@@ -2,15 +2,19 @@ import pytest
 from com.coverfox.pages.home_page import HomePage
 
 # create an instance of home page
-home_page = HomePage()
+
+
+@pytest.fixture(scope="module")
+def page_object():
+    return HomePage()
 
 
 @pytest.mark.order1
-def test_home_page_url_loads_successfully():
-    assert home_page.load()
+def test_home_page_url_loads_successfully(page_object):
+    assert page_object.load()
 
 
 @pytest.mark.order2
-def test_two_wheelers_insurance_opens_successfully():
-    home_page.open_two_wheelers_insurance()
-    assert home_page.ui.contains_text('Buying two-wheeler insurance from Coverfox is simple')
+def test_two_wheelers_insurance_opens_successfully(page_object):
+    page_object.open_two_wheelers_insurance()
+    assert page_object.ui.contains_text('Buying two-wheeler insurance from Coverfox is simple')
